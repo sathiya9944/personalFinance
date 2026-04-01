27046068a0a998cee1f02eff7f11f669a391b0c7
sathiya9944 <sathiyajothic@gmail.com>
2026-04-01 14:23:14 +0530

    Edit updated

--- backend/prisma/migrations/20260401070903_add_updated_at_to_transaction/migration.sql
+++ backend/prisma/migrations/20260401070903_add_updated_at_to_transaction/migration.sql
@@ -1,8 +1,2 @@
-/*
-  Warnings:
 
-  - Added the required column `updatedAt` to the `Transaction` table without a default value. This is not possible if the table is not empty.
-
-*/
--- AlterTable
-ALTER TABLE "Transaction" ADD COLUMN     "updatedAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP;
+ALTER TABLE "Transaction" ADD COLUMN "updatedAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP;
